2c3fa8a429fb46601ed69ee5d1af0e83675d0dd6
HeyNeek <nikoaguirre9315@gmail.com>
2023-11-25 16:31:09 -0600

    added basic write component components

--- src/components/Write/Write.jsx
+++ src/components/Write/Write.jsx
@@ -1,11 +1,31 @@
-import { Col } from "react-bootstrap";
+import { useState } from "react";
+
+import "../Write/Write.css";
 
 const Write = () => {
+  const [textAreaState, setTextAreaState] = useState("");
+  const [titleState, setTitleState] = useState("");
+
   return (
     <div className="write">
-      <Col>
-        <h1>Write here!</h1>
-      </Col>
+      <h1>Create Post</h1>
+      <input
+        className="title-area"
+        placeholder="Title of your Post"
+        type="text"
+        onChange={(e) => {
+          setTitleState(e.target.value);
+          console.log(titleState);
+        }}
+      />
+      <br />
+      <textarea
+        className="text-area"
+        onChange={(e) => {
+          setTextAreaState(e.target.value);
+          console.log(textAreaState);
+        }}
+      />
     </div>
   );
 };
