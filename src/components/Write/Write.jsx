60020857a9a2311b19c54f8b2cfa568e28010347
HeyNeek <nikoaguirre9315@gmail.com>
2023-12-22 00:36:30 -0600

    added quill to write component

--- src/components/Write/Write.jsx
+++ src/components/Write/Write.jsx
@@ -1,31 +1,21 @@
 import { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
 
 import "../Write/Write.css";
+import { useEffect } from "react";
 
 const Write = () => {
-  const [textAreaState, setTextAreaState] = useState("");
-  const [titleState, setTitleState] = useState("");
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    console.log(value);
+  }, [value]);
 
   return (
     <div className="write">
       <h1>Create Post</h1>
-      <input
-        className="title-area"
-        placeholder="Title of your Post"
-        type="text"
-        onChange={(e) => {
-          setTitleState(e.target.value);
-          console.log(titleState);
-        }}
-      />
-      <br />
-      <textarea
-        className="text-area"
-        onChange={(e) => {
-          setTextAreaState(e.target.value);
-          console.log(textAreaState);
-        }}
-      />
+      <ReactQuill className="quill-editor" value={value} onChange={setValue} />
     </div>
   );
 };
