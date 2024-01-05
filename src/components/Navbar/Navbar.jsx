5584c8b621c2974db13649da59edc3cfd8afd8bf
HeyNeek <nikoaguirre9315@gmail.com>
2024-01-04 23:06:30 -0600

    added dummy login logic + basic style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import "../Navbar/Navbar.css";
 
 import { Link, NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  //dummy function until we get user sessions with supabase
+  //need to add modal
+  const loginHandler = () => {
+    if (isLoggedIn === true) {
+      setIsLoggedIn(false);
+    } else {
+      setIsLoggedIn(true);
+    }
+  };
+
   return (
     <>
       <nav className="left-navbar">
@@ -23,6 +39,22 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
+
+        {isLoggedIn ? (
+          <ul className="navbar-selection">
+            <div className="login-handler-div" onClick={loginHandler}>
+              Log Out
+              <FontAwesomeIcon className="logout-icon" icon={faArrowRight} />
+            </div>
+          </ul>
+        ) : (
+          <ul className="navbar-selection">
+            <div className="login-handler-div" onClick={loginHandler}>
+              Log In
+              <FontAwesomeIcon className="login-icon" icon={faArrowLeft} />
+            </div>
+          </ul>
+        )}
       </nav>
     </>
   );
